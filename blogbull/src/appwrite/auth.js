ca6f5b32b18657b6abad94d2b3b68916252548e2
AbirMondal1234 <69684752+AbirMondal1234@users.noreply.github.com>
2024-04-16 17:07:02 +0530

    Auth

---
+++ blogbull/src/appwrite/auth.js
@@ -0,0 +1,59 @@
+import conf from "../conf/conf";
+
+export class AuthServices{
+    client =new this.client();
+    account;
+    
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl) // Your API Endpoint
+        .setProject(conf.appwriteProjectId); // Your project ID
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password,name}) {
+        try {
+           const userAccount= await this.account.create(ID.unique(), email,password,name);
+           if (userAccount){
+            //call another method
+            return this.login({email,password}); 
+           } else{
+            return userAccount
+           }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+           return await this.account.createEmailSession(email,password);
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+           return await this.account.get();
+           
+        } catch (error) {
+            console.log('error in getCurrentUser',error);
+        }
+        return null
+    }
+    async logOut() {
+        try {
+           return await this.account.deleteSessions();
+           
+        } catch (error) {
+            console.log('error in logout',error);
+        }
+        return null
+    }
+}
+
+const authServices =new AuthServices( );
+
+export default authServices
